0d4c294c7abf03d625f5a161d5d243c8fca0a08e
abe-one <77995299+abe-one@users.noreply.github.com>
2021-04-21 16:01:13 -0500

    build handleDelete

--- client/src/components/AddMovieForm.js
+++ client/src/components/AddMovieForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const initialFormValues = {
   title: "",
@@ -17,26 +19,97 @@ const AddMovieForm = ({}) => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post(`http://localhost:5000/api/movies`, { ...formValues, id: 23 })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      })
+      .finally(() => {});
+  };
+
   return (
     <div className="col">
       <div className="modal-content">
-        <div className="modal-header">
-          <h4 className="modal-title">
-            Adding <strong>{formValues.title}</strong>
-          </h4>
-        </div>
-        <div className="modal-body">
-          <label htmlFor="title">
-            Title
+        <form onSubmit={handleSubmit}>
+          <div className="modal-header">
+            <h4 className="modal-title">
+              Adding <strong>{formValues.title}</strong>
+            </h4>
+          </div>
+          <div className="modal-body">
+            <div className="form-group">
+              <label htmlFor="title"></label>
+              Title
+              <input
+                name="title"
+                type="text"
+                value={formValues.title}
+                onChange={handleChange}
+                placeholder="Title"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="director">Director</label>
+              <input
+                name="director"
+                type="text"
+                value={formValues.director}
+                onChange={handleChange}
+                placeholder="Director"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="genre">Genre</label>
+              <input
+                name="genre"
+                type="text"
+                value={formValues.genre}
+                onChange={handleChange}
+                placeholder="Genre"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="metascore">Metascore</label>
+              <input
+                name="metascore"
+                type="number"
+                value={formValues.metascore}
+                onChange={handleChange}
+                placeholder="Metascore"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <textarea
+                name="description"
+                value={formValues.description}
+                onChange={handleChange}
+                placeholder="Description"
+                className="form-control"
+              />
+            </div>
+          </div>
+          <div className="modal-footer">
             <input
-              name="title"
-              type="text"
-              value={formValues.title}
-              onChange={handleChange}
-              placeholder="Title"
+              type="submit"
+              className="btn btn-info"
+              value="Submit Movie"
             />
-          </label>
-        </div>
+            <Link to={"/movies"}>
+              <input type="button" className="btn btn-default" value="Cancel" />
+            </Link>
+          </div>
+        </form>
       </div>
     </div>
   );
